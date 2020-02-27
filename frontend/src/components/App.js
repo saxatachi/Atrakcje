6e1e0d9a1eae515ee1ea5e15b27c69092a5e7ab4
saxatachi <saxatachi@gmail.com>
2020-02-27 18:47:07 +0100

    Options

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import '../css/App.min.css'
+import Header from './Header'
+import Body from './Body'
+import Footer from './Footer'
+import Option from './Option'
 class App extends Component {
     render() {
         return(
-            <>
-                <h1>Jebac disa</h1>
-            </>
+            
+        <div className="app">
+            <div className="header">
+                <Header />
+            </div>
+            <div className="option">
+                <Option />
+            </div>
+            <div className="body">
+                <Body />
+            </div>
+            <div className="footer">
+                <Footer />
+            </div>
+        </div>
+        
         )
     }
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../css/Header.min.css'
+class Header extends Component {
+    render() {
+        return (
+            <div className="header__elements">
+                <div className="header__elements__title">
+                    <a>Atrakcje turystyczne miasta Lublin</a>
+                </div>
+                <div className="header__elements__search" >
+                    <div className="header__elements__search__bar">
+                            <i className="fa fa-search"></i>
+                            <input className="header__elements__search__bar__input" type="text" placeholder="Search.." name="search" />
+                        </div>
+                    </div>
+            </div>
+        );
+
+    }
+
+}
+
+export default Header;
