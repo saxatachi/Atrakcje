ba6ab566a646c09eed43b0b40749a6d72fa91432
saxatachi <saxatachi@gmail.com>
2020-03-20 11:18:07 +0100

    markers

--- frontend/src/components/Mapa.js
+++ frontend/src/components/Mapa.js
@@ -1,10 +1,12 @@
 import React, { Component, useState } from 'react';
-import {Map, Marker, Popup, TileLayer, GeoJSON, LayersControl,LayerGroup,withLeaflet,MapControl} from 'react-leaflet'
+import {Map, Marker, Popup, TileLayer, GeoJSON, LayersControl,LayerGroup,withLeaflet,MapControl,FeatureGroup, Circle } from 'react-leaflet'
+import { EditControl } from "react-leaflet-draw"
 import {getGeojson} from '../actions/geojson'
 import {connect} from 'react-redux';
 const { BaseLayer, Overlay } = LayersControl
 import MapInfo from "./MapInfo";
 import Routing from "./RoutingMachine";
+import ComponentDraw from "./Draw"
 import Road from './Road'
 class Mapa extends Component {
     constructor(props) {
@@ -21,16 +23,19 @@ class Mapa extends Component {
           coord2: [],
           coord3: [],
           coord4: [],
+          routing: false,
         }
 
         
       }
+      
       callbackcoords = (data1,data2,data3,data4) =>{
         this.setState({
           coord1: data1,
           coord2: data2,
           coord3: data3,
           coord4: data4,
+          routing: true
         })
       }
       callback = (data,datacord)=> {
@@ -69,14 +74,33 @@ class Mapa extends Component {
    
      }
       render() {
+        let pop1 = `L.latLng(${this.state.coord1[1]}, ${this.state.coord1[0]})`
+        let pop2 = `L.latLng(${this.state.coord2[1]}, ${this.state.coord2[0]})`
+        let pop3 = `L.latLng(${this.state.coord3[1]}, ${this.state.coord3[0]})`
+        let pop4 = `L.latLng(${this.state.coord4[1]}, ${this.state.coord4[0]})`
+        let pop
         
+        if(this.state.coord1.length > 0){
+          pop = pop1 + ","
+          
+        }
+        if(this.state.coord2.length > 0){
+          pop += pop2 + ","
+        }
+        if(this.state.coord3.length > 0){
+          pop += pop3 + ","
+        }
+        if(this.state.coord4.length > 0){
+          pop += pop4 
+        }
+        const correct = pop
+        console.log("correct")
+        console.log(correct)
         const { lat, lng, zoom } = this.state;
         const position = [lat,lng];
-        let name_mapa = 'dasdsa'
         return (
             
             <div className="body__elements__frame__main">
-            <h1>{name_mapa}</h1>
             <div className="body__elements__frame__options">
                 <div className="body__elements__option__road"><Road name={this.state.name} coordinates={this.state.coordinates} callbackcoords={this.callbackcoords}/></div>
             </div>
@@ -88,29 +112,42 @@ class Mapa extends Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        {/* <Routing map={this.map} /> */}
+
+        <FeatureGroup>
+          <EditControl
+      position='topleft'
+      onEdited={this._onEditPath}
+      onCreated={e => console.log(e)}
+      onDeleted={this._onDeleted}
+      draw={{
+        rectangle: false
+      }}
+    />
+    
+  </FeatureGroup>
         <Overlay name="Pomniki">
           <GeoJSONWithPomnik callbackfunc={this.callback} data={this.props.pomniki} 
            />
         </Overlay>
         <Overlay name="Siłownie">
-          <GeoJSONWithPomnik  setname={name_mapa} data={this.props.silownie}/>
+          <GeoJSONWithPomnik callbackfunc={this.callback}  data={this.props.silownie}/>
         </Overlay>
         <Overlay name="Teatry">
-          <GeoJSONWithPomnik  data={this.props.teatry}/>
+          <GeoJSONWithPomnik callbackfunc={this.callback} data={this.props.teatry}/>
         </Overlay>
         <Overlay name="Muzea">
-          <GeoJSONWithPomnik  data={this.props.muzea}/>
+          <GeoJSONWithPomnik callbackfunc={this.callback}  data={this.props.muzea}/>
         </Overlay>
         <Overlay name="Cmentarze">
-          <GeoJSONWithPomnik  data={this.props.cmentarze}/>
+          <GeoJSONWithPomnik callbackfunc={this.callback} data={this.props.cmentarze}/>
         </Overlay>
         <Overlay name="Kluby">
-          <GeoJSONWithPomnik  data={this.props.kluby}/>
+          <GeoJSONWithPomnik callbackfunc={this.callback} data={this.props.kluby}/>
         </Overlay>
         <Overlay name="Festiwale">
-          <GeoJSONWithPomnik  data={this.props.festiwale}/>
+          <GeoJSONWithPomnik callbackfunc={this.callback} data={this.props.festiwale}/>
         </Overlay>
+        {this.state.routing ? <Routing map={this.map} proba1 = {this.state.coord1} proba2={this.state.coord2} proba3={this.state.coord3} proba4={this.state.coord4}pop={pop}/>: null}
         </LayersControl>
         </Map>
           </div>
@@ -174,6 +211,7 @@ for (let key of Object.keys(properties)) {
 let tabela = table + index  + table1 + submit 
 let name_of_properties = properties.nazwa
 let feature_of_geometry = feature1[0]
+let feature_of_geometry_clear = feature1
 let popupContent = tabela;
     layer.bindPopup(popupContent);
     layer.on({
@@ -181,8 +219,21 @@ let popupContent = tabela;
         
         layer.openPopup();
         console.log(properties.nazwa)
-        props.callbackfunc(properties.nazwa,feature_of_geometry)
-        console.log(feature_of_geometry)
+        console.log("feature geometria")
+        console.log(feature_of_geometry[1])
+        if(feature_of_geometry[1] === undefined ){
+          console.log("undefined")
+          console.log(feature_of_geometry)
+          props.callbackfunc(properties.nazwa,feature_of_geometry_clear)
+        }
+        else{
+          console.log(" nie undefined")
+          console.log(feature_of_geometry)
+          props.callbackfunc(properties.nazwa,feature_of_geometry)
+        }
+        // props.callbackfunc(properties.nazwa,feature_of_geometry)
+        console.log("to jest geometria punktu")
+        console.log(feature_of_geometry_clear)
         console.log(name_of_properties)
         // props.setName = name_of_properties
         increment(name_of_properties)
--- frontend/src/components/RoutingMachine.js
+++ frontend/src/components/RoutingMachine.js
@@ -5,14 +5,43 @@ import "lrm-google";
 import { withLeaflet } from "react-leaflet";
 
 class Routing extends MapLayer {
+  
   createLeafletElement() {
     const { map } = this.props;
+    console.log("routing machine pop")
+    console.log(leafletElement)
+    console.log(this.props.pop)
+    console.log("proby")
+    console.log(this.props.proba1.length)
+    console.log(this.props.proba2.length)
+    console.log(this.props.proba3.length)
+    console.log(this.props.proba4.length)
+    let latlng1 = L.latLng(this.props.proba1[1],this.props.proba1[0])
+    let latlng2 = L.latLng(this.props.proba2[1],this.props.proba2[0])
+    let latlng3 = L.latLng(this.props.proba3[1],this.props.proba3[0])
+    let latlng4 = L.latLng(this.props.proba4[1],this.props.proba4[0])
+    let popuprouting = []
+    popuprouting.push(latlng1)
+    popuprouting.push(latlng2)
+    if(this.props.proba3.length >0){
+      popuprouting.push(latlng3)
+    }
+    if(this.props.proba4.length >0){
+      popuprouting.push(latlng4)
+    }
+    console.log("popup")
+    console.log(popuprouting)
+    console.log(L.latLng(this.props.proba2[1],this.props.proba2[0]))
+    
+    
     let leafletElement = L.Routing.control({
-      waypoints: [
-        L.latLng(51.246452, 22.568445),
-        L.latLng(51.446452, 22.868445),
-        L.latLng(51.646452, 22.668445)
-      ],
+      waypoints: popuprouting,
+      
+        // this.props.pop
+        // L.latLng(this.props.proba1[1],this.props.proba1[0]),L.latLng(this.props.proba2[1],this.props.proba2[0])
+        // L.latLng(51.2565355, 22.5648726),L.latLng(51.2416419, 22.5065198),L.latLng(51.2356221, 22.5236389),L.latLng(51.2328177, 22.5357899)
+        // L.latLng(51.2617174, 22.5315643),L.latLng(51.2435915, 22.5136702),L.latLng(51.2416419, 22.5065198),L.latLng(51.2360797, 22.5231075)
+      
       // router: new L.Routing.Google(),
       lineOptions: {
         styles: [
@@ -23,6 +52,7 @@ class Routing extends MapLayer {
           }
         ]
       },
+      language: 'pl',
       addWaypoints: false,
       draggableWaypoints: false,
       fitSelectedRoutes: false,
