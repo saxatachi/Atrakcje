0896933ddd223b0600842a67a9a63a2acde6f1c8
Michał Lejwoda <saxatachi@gmail.com>
2021-03-07 10:02:19 +0100

    Poprawa Suggestion.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <div>
-                <h1>To jest footer zly</h1>
+                <h1>To jest footer </h1>
             </div>
         );
     }
