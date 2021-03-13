fb9d3e712392d9ba09c6bb0945af30e21128a11a
Michał Lejwoda <saxatachi@gmail.com>
2021-03-13 23:21:12 +0100

    usuniecie dj_database_url

--- atrakcje/settings.py
+++ atrakcje/settings.py
@@ -11,20 +11,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url
 
 # import django_heroku
-if os.name == 'nt':
-    import platform
-    OSGEO4W = r"C:\OSGeo4W"
-    if '64' in platform.architecture()[0]:
-        OSGEO4W += "64"
-    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
-    os.environ['OSGEO4W_ROOT'] = OSGEO4W
-    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
-    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
-    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH'] 
-GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal300' 
+# if os.name == 'nt':
+#     import platform
+#     OSGEO4W = r"C:\OSGeo4W"
+#     if '64' in platform.architecture()[0]:
+#         OSGEO4W += "64"
+#     assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+#     os.environ['OSGEO4W_ROOT'] = OSGEO4W
+#     os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+#     os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+#     os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH'] 
+# GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal300' 
 # BUILD_WITH_GEO_LIBRARIES=1
 # GDAL_DATA = r'C:\Users\Michal\AppData\Local\Programs\Python\Python37\Lib\site-packages\osgeo\data\gdal'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
