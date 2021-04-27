66216d8476119710188d4ecb85dfed01e2dd956b
saxatachi <saxatachi@gmail.com>
2021-04-27 10:54:15 +0200

    debug true

--- atrakcje/settings.py
+++ atrakcje/settings.py
@@ -14,7 +14,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://atrakcje.herokuapp.com/']
 INSTALLED_APPS = [
